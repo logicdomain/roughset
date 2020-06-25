481da78f9baa4f68de81a44249e602f2a455c2d5
logic <logic>
2020-06-25 21:48:54 +0800

    init

---
+++ logic/roughset/roughset.py
@@ -0,0 +1,118 @@
+
+import pandas as pd
+
+
+class Roughset:
+    def __init__(self, U: pd.DataFrame, A: list):
+        """
+        An information system was defined as s = {U,A,V,f}.
+        U: Objects
+        A: Attributes
+        V: Values by the mapping f function.
+        f: Mapping function.
+        knowledge: U/R. R is relationship ,same as A. -> U/A
+        """
+        self.U = U
+        self.A = A
+        self.knowledge = None  # U/A
+        """
+        knowledge structure: set
+        have to be a list (type set could not be recursive) # maybe need to extend set type.
+        [element is instance of list or tuple]
+        e.g. [[1],[2],[3,4]..] or [(1),(2,3)...]
+        so as X
+        """
+        self.lower = None
+        self.upper = None
+        self.bn = None  # boundary
+        self.X = None
+        self._X_knowledge = None
+        self.core = None
+
+    @staticmethod
+    def flatten_list(l: list) -> list:
+        r = []
+        for e in l:
+            if isinstance(e, list):
+                r.extend(Roughset.flatten_list(e))
+            else:
+                r.append(e)
+        return r
+
+    @staticmethod
+    def card(l: list) -> float:
+        return len(Roughset.flatten_list(l))
+
+    @staticmethod
+    def get_knowledge(U: pd.DataFrame, A: list) -> list:
+        """
+        classify
+        generate knowledge = U/A
+        A: Attributes
+        """
+        knowledge_index_list = []
+        knowledge_grouped = U.groupby(by=A)
+        for _, group_df in knowledge_grouped:
+            knowledge_index_list.append(list(group_df.index))
+        return knowledge_index_list
+
+    def init(self) -> list:
+        """
+        for lazy load.
+        get knowledge
+        learn
+        """
+        self.knowledge = Roughset.get_knowledge(self.U, self.A)
+        return self
+
+    def set_X(self, X: pd.DataFrame):
+        self.X = X
+        knowledge = self.knowledge
+        A = self.A
+        self._X_knowledge = X_classification = Roughset.get_knowledge(X, A)
+        self._get_lower(knowledge, X_classification)
+        self._get_upper(knowledge, X_classification)
+        return self
+
+    def _get_lower(self, knowledge: list, X_classification: list) -> list:  # B_ lower boundary
+        """
+        lower index list.
+        X classification: X knowledge,X group...
+        """
+        lower_index_list = []
+
+        for k in knowledge:
+            ks = set(k)
+            for x in X_classification:
+                xs = set(x)
+                if ks.issubset(xs):
+                    lower_index_list.append(k)
+        self.lower = lower_index_list
+        return lower_index_list
+
+    def _get_upper(self, knowledge: list, X_classification: list) -> list:  # B-bar upper boundary
+        """
+        upper index list.
+        X classification: X knowledge,X group...
+        """
+        upper_index_list = []
+
+        for k in knowledge:
+            ks = set(k)
+            for x in X_classification:
+                xs = set(x)
+                if len(ks.intersection(xs)) != 0:
+                    upper_index_list.append(k)
+
+        self.upper = upper_index_list
+        return upper_index_list
+
+    def alpha(self):  # scale of rough
+        return Roughset.card(self.lower) / Roughset.card(self.upper)
+
+    def reduct(self):
+        pass
+
+    def core(self):
+        pass
+
---
+++ sample/test1.py
@@ -0,0 +1,16 @@
+from logic.roughset.roughset import Roughset as rs
+import pandas as pd
+
+df = pd.read_csv('./test_data.csv')
+print('df:\n', df,'\n')
+A = ['Headache', 'Muscle-pain', 'Temperature']
+r = rs(df, A).init()
+print('knowledge index:\n', r.knowledge,'\n')
+
+X = df[df['Flu'].str.contains('y')]
+print('X:\n', X,'\n')
+
+r.set_X(X)
+
+print('lower index:\n', r.lower,'\n')
+print('upper index:\n', r.upper,'\n')
