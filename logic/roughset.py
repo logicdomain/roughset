f2e9a212a65f0765d063eded633ced530626011b
logic <logic>
2020-12-29 19:59:56 +0800

    Update

--- logic/roughset.py
+++ logic/roughset.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+from typing import List
 
 
 class RoughSet:
@@ -24,12 +25,8 @@ class RoughSet:
         """
         self.lower = None # type: list
         self.upper = None # type: list
-        self.bn = None  # boundary, type: list
-        self.pos = None # type: list
-        self.neg = None # type: list
-        self.X = None # type: pd.DataFrame
+        self._X = None # type: pd.DataFrame
         self._X_classification = None # type: list
-        self.core = None # type: list
         self.init()
 
     @staticmethod
@@ -44,10 +41,13 @@ class RoughSet:
 
     @staticmethod
     def card(l: list) -> int:
+        """
+        # cadinality
+        """
         return len(RoughSet.flatten_list(l))
 
     @staticmethod
-    def get_classification(U: pd.DataFrame, A: list) -> list:
+    def classify(U: pd.DataFrame, A: list) -> list:
         """
         classify
         generate classification = U/A
@@ -59,28 +59,32 @@ class RoughSet:
             classification_index_list.append(list(group_df.index))
         return classification_index_list
 
-    def init(self) -> list:
+    def init(self):
         """
         for lazy load.
         get classification
         learn
         """
-        self.classification = RoughSet.get_classification(self.U, self.A)
+        self.classification = RoughSet.classify(self.U, self.A)
         return self
 
-    def set_X(self, X: pd.DataFrame):
-        self.X = X
+
+    @property
+    def X(self) -> pd.DataFrame:
+        return self._X
+
+    @X.setter
+    def X(self, X: pd.DataFrame):
+        self._X = X
+
         classification = self.classification
         A = self.A
-        self._X_classification = X_classification = RoughSet.get_classification(X, A)
+        self._X_classification = X_classification = RoughSet.classify(X, A)
         self.lower = self._get_lower(classification, X_classification)
         self.upper = self._get_upper(classification, X_classification)
-        self.bn = self.__get_bn(self.upper, self.lower)
-        self.pos = self.__get_pos(self.lower)
-        self.neg = self.__get_neg(self.U, self.upper)
         return self
 
-    def _get_lower(self, classification: list, X_classification: list) -> list:  # B_ lower boundary
+    def _get_lower(self, classification: list, X_classification: list) -> List[list]:  # B_ lower boundary
         """
         lower index list.
         X classification: X classification,X group...
@@ -95,7 +99,7 @@ class RoughSet:
                     lower_index_list.append(k)
         return lower_index_list
 
-    def _get_upper(self, classification: list, X_classification: list) -> list:  # B-bar upper boundary
+    def _get_upper(self, classification: list, X_classification: list) -> List[list]:  # B-bar upper boundary
         """
         upper index list.
         X classification: X classification,X group...
@@ -111,27 +115,39 @@ class RoughSet:
 
         return upper_index_list
 
-    def __get_bn(self, upper: list, lower: list) -> list:
+    @property
+    def bn(self) -> List[list]:
+        """
+        # boundary
+        """
+        upper = self.upper
+        lower = self.lower
         upper_list = RoughSet.flatten_list(upper)
         lower_list = RoughSet.flatten_list(lower)
         bn_list = list(set(upper_list).difference(set(lower_list)))
         return bn_list
 
-    def __get_neg(self, U: pd.DataFrame , upper:list) -> list:
+    @property
+    def neg(self) -> List[list]:
+        U = self.U
+        upper = self.upper
         all_obj_set = set(U.index)
         neg = list(all_obj_set.difference(set(RoughSet.flatten_list(upper))))
         return neg
 
-    def __get_pos(self, lower: list) -> list:
+    @property
+    def pos(self) -> List[list]:
+        lower = self.lower
         return lower
 
-
+    @property
     def alpha(self) -> float: # scale of rough
         return RoughSet.card(self.lower) / RoughSet.card(self.upper)
 
     def reduct(self):
         pass
 
+    @property
     def core(self):
         pass
 
@@ -140,11 +156,11 @@ class RoughSet:
         A = self.A.copy()
         for a in attrs:
             A.remove(a)
-        classification_new_list = RoughSet.get_classification(self.U, A)
+        classification_new_list = RoughSet.classify(self.U, A)
         return RoughSet.compare_list(classification_list, classification_new_list)
 
     @staticmethod
-    def trans_to_set(l: list) -> list:  # transform elements to set
+    def trans_to_set(l: list) -> List[list]:  # transform elements to set
         l2 = l.copy()  # use l will confuse the element (l was changing)
         for e in l:
             if isinstance(e, list):
